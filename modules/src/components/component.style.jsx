6451c6ac5ba0b43c3f4a8204a858073e0f4c1d17
dconco <onipededavid208@gmail.com>
2023-11-06 04:53:50 +0100

    Added Ionic package

--- modules/src/components/component.style.jsx
+++ modules/src/components/component.style.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 const Text = styled.p`
-    font-weight: 400;
-    width: 100%;
-    padding: 10px;
-    position: absolute;
     bottom: 0;
+    margin: 0;
+    width: 100%;
+    font-weight: 400;
+    padding: 10px 0;
+    position: absolute;
     text-align: center;
     background-color: blueviolet;
     color: ${ props => props.color || 'whitesmoke'};
